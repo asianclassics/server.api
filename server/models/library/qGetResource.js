88b28a8e3966420740968285ab208a60860775cd
joelcrawford <joelrootcrawford@gmail.com>
2020-10-29 12:36:51 -0700

    resource endpoint parsers update

--- server/models/library/qGetResource.js
+++ server/models/library/qGetResource.js
@@ -3,6 +3,7 @@ const { setIndex } = require('../../parsers/setIndex')
 
 // next refactor the build query section, combine model with query builder for endpoints
 // maybe each endpoint is its own folder under model?
+// or something else
 module.exports = {
     getResource(params) {
         const index = setIndex(params)
